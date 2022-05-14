04359f675282bf83b77032e27c3291b9bae92b35
AtomicGrog <paul.rance@googlemail.com>
2022-05-14 14:59:59 +1000

    removed duplicate overrides causing forced true... wonder about others having same issue.

--- hud/init.lua
+++ hud/init.lua
@@ -51,7 +51,7 @@ local function save()
             toolbar.changeColor(address, data.backgroundColor, data.primaryColor, data.accentColor)
         end
         if data.notifications then
-            table.insert(notificationsUsers, {component.proxy(address), {data.xRes or 2560, data.yRes or 1440}, data.scale or 3, data.offset or 0, data.backgroundColor or colors.darkGray, data.primaryColor or colors.electricBlue, data.accentColor or colors.accentColor, data.disabledColor or colors.disabledColor, data.maintColor or colors.maintColor})
+            table.insert(notificationsUsers, {component.proxy(address), {data.xRes or 2560, data.yRes or 1440}, data.scale or 3, data.offset or 0, data.backgroundColor or colors.darkGray, data.primaryColor or colors.electricBlue, data.accentColor or colors.accentColor, data.disabledColor or colors.disabledColor, data.maintColor or colors.maintColor, data.displayDisabled})
             notifications.changeColor(address, data.backgroundColor, data.primaryColor, data.accentColor, data.disabledColor, data.maintColor, data.displayDisabled)
         end
     end
